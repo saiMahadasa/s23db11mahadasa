50d87aa41ed4ace3e98a6862368718466ff4efbb
saiMahadasa <saimahadasa1999@gmail.com>
2023-11-28 17:35:24 -0600

    aeroplane schema validation

--- models/aeroplane.js
+++ models/aeroplane.js
@@ -1,8 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const aeroplaneSchema = mongoose.Schema({
-    aeroplane_type: String,
-    color: String,
-    number_of_wings: Number
-})
-module.exports = mongoose.model("Aeroplane",
-aeroplaneSchema)
+    aeroplane_type: {
+        required: true,
+        type: String,
+    },
+    color: {
+        required: true,
+        type: String,
+    },
+    number_of_wings: {
+        type: Number,
+        required: true,
+        min: [2, 'The number of wings must be at least 2.'],
+        max: [6, 'The number of wings cannot exceed 6.']
+    }
+});
+
+module.exports = mongoose.model("Aeroplane", aeroplaneSchema);
